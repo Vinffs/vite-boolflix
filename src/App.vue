bb845e3c48fdfd6fce1128d65e4239fa32f0a10d
Vinffs <vin.demiranda@gmail.com>
2023-11-15 17:07:53 +0100

    ticket-1

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,40 @@
 <template>
+  <HeaderComponent @clicked="getMoviesAndSeries" />
   <main>
-    <section id="movie" class="container">
-      <h2>Movies</h2>
-      <div class="row">
-        <div class="col-12 col-md-4 col-lg-3" v-for="(movie, index) in store.movieList" :key="movie.id">
-          {{ movie.title }}
+    <div class="container" v-if="(!store.movieList.length && !store.seriesList.length)">
+      <h1>Search for Movies and TV Series</h1>
+    </div>
+    <div v-else>
+      <section id="movie" class="container">
+        <h2>Movies</h2>
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-3" v-for="(movie, index) in store.movieList" :key="movie.id">
+            {{ movie.title }}
+          </div>
         </div>
-      </div>
-    </section>
-    <section id="series" class="container">
-      <h2>TV Series</h2>
-      <div class="row">
-        <div class="col-12 col-md-4 col-lg-3" v-for="(serie, index) in store.seriesList" :key="serie.id">
-          {{ serie.name }}
+      </section>
+      <section id="series" class="container">
+        <h2>TV Series</h2>
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-3" v-for="(serie, index) in store.seriesList" :key="serie.id">
+            {{ serie.name }}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
+
   </main>
 </template>
 
 <script>
 import { store } from "./assets/data/store";
 import axios from 'axios';
+import HeaderComponent from "./components/HeaderComponent.vue";
 export default {
   name: 'App',
+  components: {
+    HeaderComponent,
+  },
   data() {
     return {
       store,
@@ -36,8 +47,10 @@ export default {
       const params = this.store.params;
 
       Promise.all([axios.get(urlMovies, { params: params }), axios.get(urlSeries, { params: params })]).then((results) => {
+        console.log(this.store.movieList, this.store.seriesList)
         this.store.movieList = results[0].data.results;
         this.store.seriesList = results[1].data.results;
+        console.log(this.store.movieList, this.store.seriesList)
       })
     }
   },
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="row m-0">
+    <div class="col-12 d-flex justify-content-center align-items-end pb-5">
+      <div class="d-flex">
+        <input type="text" class="form-control" placeholder="Search..." />
+        <button class="ms-2 btn btn-danger" type="button" @click="$emit('clicked')">
+          Search
+        </button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { store } from "../assets/data/store";
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      store,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
